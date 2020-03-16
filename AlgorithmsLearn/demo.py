f113b180e6edcd83aa3e815af24686dba90abce2
G-linch <514292298@qq.com>
2020-03-16 23:11:41 +0800

    生成器相关
    生成器的特点
    杨辉三角实现
    斐波那契数列实现

--- AlgorithmsLearn/demo.py
+++ AlgorithmsLearn/demo.py
@@ -38,7 +38,7 @@ def fact_iter(num, product):
 n = fact_iter(100,1)
 print(n)
 
-'''
+
 
 a = list(range(1,100))
 print(a[2:10])
@@ -66,4 +66,64 @@ print(b)
 
 
 #生成器
+li = [x for x in range(1,100) if x%2 == 1]
+#list区别在于（）保存的是算法
+gene = (x for x in range(1,100) if x%2 == 0)
+for i in range(10):
+    print(next(gene))
+for i in gene:
+    print(i)
+
+#斐波那契数列
+def fib(max):
+    n,a,b = 0,0,1
+    while max > n:
+        print(b)
+        a,b = b,a+b
+        n = n + 1
+    return 'Done'
+
+#将以上函数转变成generator
+def fib(max):
+    n,a,b = 0,0,1
+    while max > n:
+        yield b
+        a,b = b,a+b
+        n = n + 1
+    return 'Done'
+a = fib(5)
+print(a)
+'''
+
+#杨辉三角
+'''
+第一行  [1,2,1]
+第二行计算方式  0,1,2,1
+              +1,2,1,0
+              =1,3,3,1
+              可以看出是在给第一行的list前后补零相加而成的
+'''
+def triangeles(n):
+    b = [1]
+    while len(b) < n:
+        print(b)
+        c = [0]+b
+        d = b+[0]
+        b = [c[i]+d[i] for i in range(len(c))]
+triangeles(10)
+
+def triangels():
+    b = [1]
+    while True:
+        yield b
+        c = [0] + b
+        d = b + [0]
+        b = [c[i]+d[i] for i in range(len(c))]
+n = 1
+for i in triangels():
+    n = n + 1
+    print(i)
+    if n > 10:
+        break
+
 
